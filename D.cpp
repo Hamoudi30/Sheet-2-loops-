948c3ca701135b79c0b3e3f11c5a8960f47a17a0
Hamoudi30 <64968485+Hamoudi30@users.noreply.github.com>
2021-11-13 01:11:12 +0200

    Create D.cpp

---
+++ D.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    while (true) {
+        int num;
+        cin >> num;
+        if (num == 1999) {
+            cout << "Correct\n";
+            break;
+        } else {
+            cout << "Wrong\n";
+        }
+        num++;
+    }
+    return 0;
+}
